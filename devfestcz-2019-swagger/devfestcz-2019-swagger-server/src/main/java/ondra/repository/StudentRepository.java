3ad2030d475f8a4f5135ca7cd4d2922cbd68c6fd
Ondra Kučera <ondrej.kucera@centrum.cz>
2019-09-11 17:13:29 +0200

    Add StudentRepository

---
+++ devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/main/java/ondra/repository/StudentRepository.java
@@ -0,0 +1,14 @@
+package ondra.repository;
+
+import ondra.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+	List<Student> findAllByOrderByLastNameAscFirstNameAscYearAsc();
+
+}
