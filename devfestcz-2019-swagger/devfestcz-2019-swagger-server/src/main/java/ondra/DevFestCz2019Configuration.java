9e9473f19be5cfa05ef1bc74c13efbb37922d8b1
Ondra Kučera <ondrej.kucera@centrum.cz>
2019-09-11 15:39:52 +0200

    Add Spring configuration

---
+++ devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/main/java/ondra/DevFestCz2019Configuration.java
@@ -0,0 +1,9 @@
+package ondra;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class DevFestCz2019Configuration {
+}
