46463b83fe408327a39521c2ee409d3f9c859859
Ondra Kučera <ondrej.kucera@centrum.cz>
2019-09-11 21:35:01 +0200

    Add generating of JS client

--- devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/swagger/generate-classes.sh
+++ devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/swagger/generate-classes.sh
@@ -5,3 +5,8 @@ set -e
 swagger-codegen generate -l spring --api-package ondra.restapi.controller --model-package ondra.restapi.dto --model-name-suffix Dto -DdelegatePattern=true,hideGenerationTimestamp=true,interfaceOnly=true,java8=true -i swagger.yaml -o output
 cp -rf output/src/main ..
 rm -rf output
+
+swagger-codegen generate -l javascript --api-package client --model-package dto --model-name-suffix Dto -DusePromises=true,hideGenerationTimestamp=true,useES6=true -i swagger.yaml -o output
+mkdir -p ../../../devfestcz-2019-swagger-client/src/restapi/
+cp -rf output/src/* ../../../devfestcz-2019-swagger-client/src/restapi
+rm -rf output
