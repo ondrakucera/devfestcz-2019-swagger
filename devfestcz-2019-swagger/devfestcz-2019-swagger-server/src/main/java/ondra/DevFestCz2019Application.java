f5f1cafb34c5ff0966a9789ef3f5cd05e7ddc64e
Ondra Kučera <ondrej.kucera@centrum.cz>
2019-09-11 14:24:56 +0200

    Add initial project structure

---
+++ devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/main/java/ondra/DevFestCz2019Application.java
@@ -0,0 +1,13 @@
+package ondra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevFestCz2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevFestCz2019Application.class, args);
+	}
+
+}
