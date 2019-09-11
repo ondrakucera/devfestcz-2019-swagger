e29f2f4e18a9c9c75f337b265f3e238e539fde5d
Ondra Kučera <ondrej.kucera@centrum.cz>
2019-09-11 18:23:51 +0200

    Improve generate-classes.sh

--- devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/swagger/generate-classes.sh
+++ devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/swagger/generate-classes.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+set -e
+
 swagger-codegen generate -l spring --api-package ondra.restapi.controller --model-package ondra.restapi.dto --model-name-suffix Dto -DdelegatePattern=true,hideGenerationTimestamp=true,interfaceOnly=true,java8=true -i openapi.yaml -o output
+cp -rf output/src/main ..
+rm -rf output
