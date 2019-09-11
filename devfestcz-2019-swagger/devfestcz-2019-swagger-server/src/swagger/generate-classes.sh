d6cdb20afded5077d5cedcd82e15baa7e718439c
Ondra Kučera <ondrej.kucera@centrum.cz>
2019-09-11 21:03:55 +0200

    Rewrite API to Swagger 2.0
    The sequence of reasons for this is the following. Swagger codegen 3.x
    dropped support for several languages, most notably all JavaScript
    clients. Swagger codegen 2.x, however, doesn't support Open API 3.0
    format. So, in order to generate a JavaScript client, we have to use the
    older codegen, which implies using the older format.

--- devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/swagger/generate-classes.sh
+++ devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/swagger/generate-classes.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-swagger-codegen generate -l spring --api-package ondra.restapi.controller --model-package ondra.restapi.dto --model-name-suffix Dto -DdelegatePattern=true,hideGenerationTimestamp=true,interfaceOnly=true,java8=true -i openapi.yaml -o output
+swagger-codegen generate -l spring --api-package ondra.restapi.controller --model-package ondra.restapi.dto --model-name-suffix Dto -DdelegatePattern=true,hideGenerationTimestamp=true,interfaceOnly=true,java8=true -i swagger.yaml -o output
 cp -rf output/src/main ..
 rm -rf output
