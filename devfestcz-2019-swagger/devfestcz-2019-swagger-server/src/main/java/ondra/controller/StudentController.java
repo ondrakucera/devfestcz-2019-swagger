5c29637b6c340dbb49066b73c866011e4881ef8e
Ondra Kučera <ondrej.kucera@centrum.cz>
2019-09-11 17:40:14 +0200

    Implement StudentController.getStudent

--- devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/main/java/ondra/controller/StudentController.java
+++ devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/main/java/ondra/controller/StudentController.java
@@ -1,10 +1,14 @@
 package ondra.controller;
 
 import ondra.converter.StudentConverter;
+import ondra.domain.StudentNotFoundException;
 import ondra.restapi.controller.StudentsApi;
 import ondra.restapi.dto.StudentDto;
 import ondra.service.StudentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,6 +27,16 @@ public class StudentController implements StudentsApi {
 	@Override
 	public ResponseEntity<List<StudentDto>> getStudents() {
 		return ResponseEntity.ok(studentConverter.toDtos(studentService.getAllStudents()));
+	}
+
+	@Override
+	public ResponseEntity<StudentDto> getStudent(Integer studentId) {
+		return ResponseEntity.ok(studentConverter.toDto(studentService.getStudent(studentId)));
+	}
+
+	@ResponseStatus(value= HttpStatus.NOT_FOUND)
+	@ExceptionHandler(StudentNotFoundException.class)
+	public void handleStudentNotFound() {
 	}
 
 }
