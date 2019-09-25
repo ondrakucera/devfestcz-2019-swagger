f7d1d38dc2ddff857807df98b3d8193c3c6bdff8
Ondra Kučera <ondrej.kucera@centrum.cz>
2019-09-25 10:12:08 +0200

    Add devfestcz2019-swagger-client-angular as generated by ng new

---
+++ devfestcz-2019-swagger/devfestcz2019-swagger-client-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'devfestcz2019-swagger-client-angular';
+}
