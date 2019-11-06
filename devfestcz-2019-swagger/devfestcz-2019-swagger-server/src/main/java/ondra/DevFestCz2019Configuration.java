f07155089ee7df1ed4fd852186bbb33bb3ee5eb2
Ondra Kučera <ondrej.kucera@centrum.cz>
2019-11-06 23:18:12 +0100

    Add Swagger UI

--- devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/main/java/ondra/DevFestCz2019Configuration.java
+++ devfestcz-2019-swagger/devfestcz-2019-swagger-server/src/main/java/ondra/DevFestCz2019Configuration.java
@@ -1,12 +1,19 @@
 package ondra;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @ComponentScan
+@EnableSwagger2
 public class DevFestCz2019Configuration implements WebMvcConfigurer {
 
 	@Override
@@ -14,6 +21,15 @@ public class DevFestCz2019Configuration implements WebMvcConfigurer {
 		registry.addMapping("/**")
 				.allowedOrigins("*")
 				.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("ondra.controller"))
+				.build()
+				.apiInfo(new ApiInfoBuilder().build());
 	}
 
 }
