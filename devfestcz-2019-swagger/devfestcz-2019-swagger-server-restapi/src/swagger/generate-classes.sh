4289685a1a431229e6f4b4564c165b892fbe4eb6
Ondra Kučera <ondrej.kucera@centrum.cz>
2019-11-08 06:28:33 +0100

    BE: Add devfestcz-2019-swagger-server-restapi module

--- devfestcz-2019-swagger/devfestcz-2019-swagger-server-restapi/src/swagger/generate-classes.sh
+++ devfestcz-2019-swagger/devfestcz-2019-swagger-server-restapi/src/swagger/generate-classes.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-swagger-codegen generate -l spring --api-package ondra.restapi.controller --model-package ondra.restapi.dto --model-name-suffix Dto -DdelegatePattern=true,hideGenerationTimestamp=true,interfaceOnly=true,java8=true -i swagger.yaml -o output
-cp -rf output/src/main ..
-rm -rf output
-
 swagger-codegen generate -l typescript-fetch --api-package client --model-package dto --model-name-suffix Dto -DsupportsES6=true -i swagger.yaml -o output
 mkdir -p ../../../devfestcz-2019-swagger-client/src/restapi/
 cp -rf output/*.ts ../../../devfestcz-2019-swagger-client/src/restapi
